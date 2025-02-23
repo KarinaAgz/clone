63276dcf329c857a8b711cabd8cad9baf2259860
KarinaAgz <karinaangon10@gmail.com>
2025-02-23 00:05:40 +0000

    clone vista main

---
+++ project1/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("logali.group.project1.controller.App", {
+        onInit() {
+        }
+    });
+});
---
+++ project1/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["logali/group/project1/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
